63e68f684fa7994226ef70a8cd2877f8ae74f5e1
Seth <itz.saga@gmail.com>
2017-09-01 23:14:51 -0500

    Update index with new fork repo.

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function showResults(json) {
 }
 
 function forkRepo() {
-  const repo = 'learn-co-curriculum/javascript-fetch-lab'
+  const repo = 'itzsaga/fcc-sept-2017'
   //use fetch to fork it!
 }
 
